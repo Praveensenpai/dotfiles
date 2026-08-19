c74057de9c985eb82ea830394b5210a085ab8382
Praveen Senpai <pvnt20@gmail.com>
2026-08-19 16:55:32 +0530

    style(anime4k): polish install_anime4k.sh with banner and argument forwarding

--- scripts/install_anime4k.sh
+++ scripts/install_anime4k.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-curl -fsSL -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/Praveensenpai/anime4k-cli/main/install.sh" | sh
+#!/bin/bash
+
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+BOLD='\033[1m'
+NC='\033[0m'
+
+echo -e "${PURPLE}📺 Installing & configuring Anime4K for mpv via anime4k-cli...${NC}\n"
+
+curl -4 -fsSL -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/Praveensenpai/anime4k-cli/main/install.sh?v=$(date +%s)" | sh -s -- "$@"
